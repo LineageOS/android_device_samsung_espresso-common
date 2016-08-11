1f0c4ac69a868f8465bc3694a941dc429b962c3b
Andreas Blaesius <skate4life@gmx.de>
2016-08-11 18:20:17 +0200

    Cleanup, reorder files and sync with AOSP
    Change-Id: I21617b6873fbbfcc147f8aa137d9f5be2052924f

---
+++ BoardConfigCM.mk
@@ -0,0 +1,23 @@
+# Boot Animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# RIL
+BOARD_VENDOR := samsung
+BOARD_PROVIDES_LIBRIL := true
+BOARD_MODEM_TYPE := xmm6260
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 12
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
+
+# Recovery
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/f_mass_storage/lun0/file"
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/espresso-common
+
+# Charging mode
+BOARD_CHARGER_RES := device/samsung/espresso-common/res/charger
