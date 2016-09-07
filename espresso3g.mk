f78230bcbacede912ee4cc6100bf2adffdcee03e
Ziyan <jaraidaniel@gmail.com>
2016-09-08 01:23:49 +0200

    ril: add ril shim
    Based on https://github.com/OMAP4-AOSP/android_device_samsung_tuna/tree/aosp-6.0/ril/libsecril-shim.
    
    KitKat:
    fix compiling
    
    Change-Id: I8a21e9100938903d593ad67e0a9574c622d5dab4

--- espresso3g.mk
+++ espresso3g.mk
@@ -2,6 +2,7 @@ $(call inherit-product, device/samsung/espresso-common/espresso-common.mk)
 
 PRODUCT_PACKAGES += \
     libsecril-client \
+    libsecril-shim \
     libsecnativefeature
 
 PRODUCT_PROPERTY_OVERRIDES += \
