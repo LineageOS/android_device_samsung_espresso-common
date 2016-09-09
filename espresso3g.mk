6a2420d3bb93c7af927fd46cb2fb2b68b25114fa
Ziyan <jaraidaniel@gmail.com>
2016-09-09 18:04:54 +0200

    ril: add open-sourced libsecnativefeature, used by libsec-ril
    Change-Id: I2f487db23b31c63ba919863f393ca37cc9a81191

--- espresso3g.mk
+++ espresso3g.mk
@@ -2,7 +2,8 @@ $(call inherit-product, device/samsung/espresso-common/device-common.mk)
 
 # RIL
 PRODUCT_PACKAGES += \
-    libsecril-client
+    libsecril-client \
+    libsecnativefeature
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungOmap4RIL
