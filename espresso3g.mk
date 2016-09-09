b5d273cca7fd24ad4b984abc02f12dcfae6b64b3
Ziyan <jaraidaniel@gmail.com>
2016-09-09 18:04:54 +0200

    ril: add open-sourced libsecnativefeature, used by libsec-ril
    Change-Id: I2f487db23b31c63ba919863f393ca37cc9a81191

--- espresso3g.mk
+++ espresso3g.mk
@@ -1,4 +1,7 @@
 $(call inherit-product, device/samsung/espresso-common/espresso-common.mk)
 
+PRODUCT_PACKAGES += \
+    libsecnativefeature
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL
