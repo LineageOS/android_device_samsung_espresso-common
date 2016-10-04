2304e45f4255f25c8d40a95b8b88c39a0262736c
Andreas Blaesius <skate4life@gmx.de>
2016-10-04 19:53:34 +0200

    espresso-common: compile libsecril-client on 3g variants
    - got removed by accident on "espresso-common: add opensource sensor hal"
    
    Change-Id: I07143863ab0cd33b4ec91d9208821c37b716bfbe

--- espresso3g.mk
+++ espresso3g.mk
@@ -1,6 +1,7 @@
 $(call inherit-product, device/samsung/espresso-common/espresso-common.mk)
 
 PRODUCT_PACKAGES += \
+    libsecril-client \
     libsecnativefeature
 
 PRODUCT_PROPERTY_OVERRIDES += \
